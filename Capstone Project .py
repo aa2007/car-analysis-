b50babcd6ebfbc3d2176b4d12a456ff10a9d44cf
aa2007 <66526032+aa2007@users.noreply.github.com>
2020-09-02 16:47:28 -0400

    Add files via upload

---
+++ Capstone Project .py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notebook will be used mainly used for the capstone project.
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello capstone project course.")
+
+
+# In[ ]:
+
+
+
+
